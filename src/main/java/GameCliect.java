f7d6afef70d95b53c7121e2be90edaf009dd74fc
Vicky <t102360048@ntut.org.tw>
2020-12-19 13:44:00 +0800

    新增視窗設計Ok

--- src/main/java/GameCliect.java
+++ src/main/java/GameCliect.java
@@ -1,5 +1,18 @@
-public class GameCliect {
-    public static void main(String[] args) {
-        System.out.println("Tank war!");
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCliect extends JComponent {
+
+    private int ScreenWidth;
+    private int ScreenHeight;
+    GameCliect() {
+        this.setPreferredSize(new Dimension(800,600));
+    }
+
+    public GameCliect(int screenWidth, int screenHeight) {
+        ScreenWidth = screenWidth;
+        ScreenHeight = screenHeight;
+        this.setPreferredSize(new Dimension(ScreenWidth,ScreenHeight));
+
     }
 }
--- src/main/java/TankGame.java
+++ src/main/java/TankGame.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class TankGame {
+    public static void main(String[] args) {
+        GameCliect gameCliect =new GameCliect(1024,768);
+        JFrame jFrame =new JFrame();
+        jFrame.add(gameCliect);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+    }
 }
